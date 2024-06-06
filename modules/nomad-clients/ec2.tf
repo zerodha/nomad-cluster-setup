5d457d69eb1301063ab00fb874b9ce988cae09f6
C V Hariharan <hariharan@zerodha.com>
2024-06-06 12:24:39 +0530

    feat: add support for custom cloudinit user script

--- modules/nomad-clients/ec2.tf
+++ modules/nomad-clients/ec2.tf
@@ -28,18 +28,7 @@ resource "aws_instance" "nomad_client" {
     instance_metadata_tags      = "enabled"
   }
 
-  user_data_base64 = base64encode(templatefile("${path.module}/scripts/setup_client.tftpl.sh", {
-    route_53_resolver_address      = var.route_53_resolver_address
-    enable_docker_plugin           = var.enable_docker_plugin
-    nomad_join_tag_key             = "nomad_ec2_join"
-    nomad_join_tag_value           = var.nomad_join_tag_value
-    nomad_file_limit               = var.nomad_file_limit
-    nomad_client_exec_host_volumes = var.nomad_client_exec_host_volumes
-    nomad_client_cfg = templatefile("${path.module}/templates/nomad.tftpl", {
-      nomad_dc         = var.cluster_name
-      nomad_acl_enable = var.nomad_acl_enable
-    })
-  }))
+  user_data_base64 = base64encode(data.cloudinit_config.config.rendered)
 
   tags = {
     Name           = "${var.client_name}-${count.index + 1}"
--- modules/nomad-servers/launch_template.tf
+++ modules/nomad-servers/launch_template.tf
@@ -8,21 +8,7 @@ resource "aws_launch_template" "nomad_server" {
   vpc_security_group_ids  = concat(var.default_security_groups, [aws_security_group.nomad_agent.id])
   update_default_version  = true
 
-  user_data = base64encode(templatefile("${path.module}/scripts/setup_server.tftpl.sh", {
-    nomad_acl_bootstrap_token = var.nomad_acl_bootstrap_token
-    nomad_acl_enable          = var.nomad_acl_enable
-    nomad_file_limit          = var.nomad_file_limit
-    nomad_server_cfg = templatefile("${path.module}/templates/nomad.tftpl", {
-      nomad_dc                 = var.cluster_name
-      aws_region               = var.aws_region
-      nomad_bootstrap_expect   = var.nomad_bootstrap_expect
-      nomad_gossip_encrypt_key = var.nomad_gossip_encrypt_key
-      nomad_join_tag_key       = "nomad_ec2_join"
-      nomad_join_tag_value     = var.nomad_join_tag_value
-      nomad_acl_enable         = var.nomad_acl_enable
-    })
-    nomad_file_limit = var.nomad_file_limit
-  }))
+  user_data = base64encode(data.cloudinit_config.config.rendered)
 
   metadata_options {
     http_endpoint               = "enabled"
