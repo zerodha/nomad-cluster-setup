94dcbf45087782935454d2db6138114628eb8ffe
Ameya Shenoy <shenoy.ameya@gmail.com>
2026-02-10 13:21:32 +0530

    feat: add http_max_conns_per_client
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- modules/nomad-servers/locals.tf
+++ modules/nomad-servers/locals.tf
@@ -7,13 +7,15 @@ locals {
     nomad_raft_backup_bucket  = var.nomad_raft_backup_bucket
 
     nomad_server_cfg = templatefile("${path.module}/templates/nomad.tftpl", {
-      nomad_dc                 = var.cluster_name
-      aws_region               = var.aws_region
-      nomad_bootstrap_expect   = var.nomad_bootstrap_expect
-      nomad_gossip_encrypt_key = var.nomad_gossip_encrypt_key
-      nomad_join_tag_key       = "nomad_ec2_join"
-      nomad_join_tag_value     = var.nomad_join_tag_value
-      nomad_acl_enable         = var.nomad_acl_enable
+      nomad_dc                        = var.cluster_name
+      aws_region                      = var.aws_region
+      nomad_bootstrap_expect          = var.nomad_bootstrap_expect
+      nomad_gossip_encrypt_key        = var.nomad_gossip_encrypt_key
+      nomad_join_tag_key              = "nomad_ec2_join"
+      nomad_join_tag_value            = var.nomad_join_tag_value
+      nomad_acl_enable                = var.nomad_acl_enable
+      enable_mem_oversubscription     = var.enable_mem_oversubscription
+      nomad_http_max_conns_per_client = var.http_max_conns_per_client
     })
     nomad_file_limit = var.nomad_file_limit
   })
