05fe0537f0856e7fc584ec28ad3559c3af5bb75f
C V Hariharan <hariharan@zerodha.com>
2026-05-15 15:42:38 +0530

    fix: gc interval quote

--- modules/nomad-clients/scripts/setup_client.tftpl.sh
+++ modules/nomad-clients/scripts/setup_client.tftpl.sh
@@ -187,7 +187,7 @@ client {
   server_join {
     retry_join = ["provider=aws region=${aws_region} tag_key=${nomad_join_tag_key} tag_value=${nomad_join_tag_value}"]
   }
-  gc_interval              = ${nomad_gc_interval}
+  gc_interval              = "${nomad_gc_interval}"
   gc_disk_usage_threshold  = ${nomad_gc_disk_usage_threshold}
   gc_inode_usage_threshold = ${nomad_gc_inode_usage_threshold}
   gc_max_allocs            = ${nomad_gc_max_allocs}
